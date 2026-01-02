ba0729f3dcb699ff3957baafc0f611ff79e468dc
Michael Pacheco <mdpgd95@gmail.com>
2026-01-02 08:15:29 -0300

    refactor: move config to separate file

---
+++ src/Constants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace App::Config {
+namespace Window {
+constexpr auto TITLE = "Minecraft";
+constexpr int WIDTH = 800;
+constexpr int HEIGHT = 600;
+constexpr float CLEAR_COLOR[] = {0.1, 0.1, 0.1};
+} // namespace Window
+} // namespace App::Config
--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,53 @@
 #define SDL_MAIN_USE_CALLBACKS 1
+
+#include "Constants.h"
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
 static SDL_Window *window = nullptr;
 static SDL_Renderer *renderer = nullptr;
 
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
-{
-    SDL_SetAppMetadata("Minecraft", "0.1.0", "com.example.minecraft");
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+  SDL_SetAppMetadata("Minecraft", "0.1.0", "com.example.minecraft");
 
-    if (!SDL_Init(SDL_INIT_VIDEO))
-    {
-        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
 
-    if (!SDL_CreateWindowAndRenderer("Minecraft", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer))
-    {
-        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
+  if (!SDL_CreateWindowAndRenderer("Minecraft", App::Config::Window::WIDTH,
+                                   App::Config::Window::HEIGHT,
+                                   SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
 
-    SDL_SetRenderLogicalPresentation(renderer, 640, 480, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+  SDL_SetRenderLogicalPresentation(renderer, App::Config::Window::WIDTH,
+                                   App::Config::Window::HEIGHT,
+                                   SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
-    return SDL_APP_CONTINUE;
+  return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-{
-    if (event->type == SDL_EVENT_QUIT)
-    {
-        return SDL_APP_SUCCESS;
-    }
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+  if (event->type == SDL_EVENT_QUIT) {
+    return SDL_APP_SUCCESS;
+  }
 
-    return SDL_APP_CONTINUE;
+  return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
-    SDL_SetRenderDrawColorFloat(renderer, 0.1f, 0.1f, 0.1f, SDL_ALPHA_OPAQUE_FLOAT);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+SDL_AppResult SDL_AppIterate(void *appstate) {
+  constexpr auto red = App::Config::Window::CLEAR_COLOR[0];
+  constexpr auto green = App::Config::Window::CLEAR_COLOR[0];
+  constexpr auto blue = App::Config::Window::CLEAR_COLOR[0];
 
-    return SDL_APP_CONTINUE;
+  SDL_SetRenderDrawColorFloat(renderer, red, green, blue,
+                              SDL_ALPHA_OPAQUE_FLOAT);
+  SDL_RenderClear(renderer);
+  SDL_RenderPresent(renderer);
+
+  return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
-{
-}
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {}
