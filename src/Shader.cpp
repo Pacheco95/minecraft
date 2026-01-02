6abef55f90322d54fdabffa4ecc1b39dc2c93ae5
Michael Pacheco <mdpgd95@gmail.com>
2026-01-02 15:23:06 -0300

    feat: add shader class

---
+++ src/Shader.cpp
@@ -0,0 +1,81 @@
+#include "Shader.h"
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+namespace App {
+
+std::string loadShaderFile(const char *path) {
+  std::string content;
+  std::ifstream file;
+
+  file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+  try {
+    file.open(path);
+    std::stringstream stream;
+    stream << file.rdbuf();
+    file.close();
+
+    content = stream.str();
+  } catch (std::ifstream::failure &e) {
+    SPDLOG_ERROR("ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: {} {}", path, e.what());
+  }
+
+  return content;
+}
+
+Shader::Shader(const char *vertexPath, const char *fragmentPath) {
+  const std::string vertexCode = loadShaderFile(vertexPath);
+  const std::string fragmentCode = loadShaderFile(fragmentPath);
+
+  const uint vertex = compile(vertexCode, ShaderType::VERTEX);
+  const uint fragment = compile(fragmentCode, ShaderType::FRAGMENT);
+
+  // shader Program
+  ID = glCreateProgram();
+  glAttachShader(ID, vertex);
+  glAttachShader(ID, fragment);
+  glLinkProgram(ID);
+
+  checkCompileErrors(ID, ShaderType::PROGRAM);
+
+  // delete the shaders as they're linked into our program now and no longer necessary
+  glDeleteShader(vertex);
+  glDeleteShader(fragment);
+}
+
+void Shader::checkCompileErrors(const GLuint shader, const ShaderType type) {
+  GLint success;
+  constexpr uint bufferSize = 1024;
+  static GLchar errorMessage[bufferSize];
+
+  const auto index = static_cast<uint>(type);
+  const auto shaderTypeStr = shaderTypes[index];
+
+  if (type != ShaderType::PROGRAM) {
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+      glGetShaderInfoLog(shader, bufferSize, nullptr, errorMessage);
+      SPDLOG_ERROR("ERROR::SHADER_COMPILATION_ERROR of type: {}\n\t{}", shaderTypeStr, errorMessage);
+    }
+  } else {
+    glGetProgramiv(shader, GL_LINK_STATUS, &success);
+    if (!success) {
+      glGetProgramInfoLog(shader, bufferSize, nullptr, errorMessage);
+      SPDLOG_ERROR("ERROR::PROGRAM_LINKING_ERROR of type: {}\n\t{}", shaderTypeStr, errorMessage);
+    }
+  }
+}
+
+uint Shader::compile(const std::string &code, const ShaderType type) {
+  const uint shaderId = glCreateShader(type == ShaderType::VERTEX ? GL_VERTEX_SHADER : GL_FRAGMENT_SHADER);
+  const auto shaderCodePtr = code.c_str();
+  glShaderSource(shaderId, 1, &shaderCodePtr, nullptr);
+  glCompileShader(shaderId);
+  checkCompileErrors(shaderId, type);
+  return shaderId;
+}
+
+} // namespace App
---
+++ src/Shader.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <string>
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <spdlog/spdlog.h>
+
+namespace App {
+enum class ShaderType {
+  VERTEX,
+  FRAGMENT,
+  PROGRAM
+};
+
+constexpr const char *const shaderTypes[] = {"VERTEX", "FRAGMENT", "PROGRAM"};
+
+class Shader {
+public:
+  unsigned int ID;
+
+  Shader(const char *vertexPath, const char *fragmentPath);
+
+  void use() const {
+    glUseProgram(ID);
+  }
+
+  void set(const std::string &name, const bool value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), static_cast<int>(value));
+  }
+
+  void set(const std::string &name, const int value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+  }
+
+  void set(const std::string &name, const float value) const {
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+  }
+
+  void set(const std::string &name, const glm::vec2 &value) const {
+    glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+  }
+
+  void set(const std::string &name, const float x, const float y) const {
+    glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
+  }
+
+  void set(const std::string &name, const glm::vec3 &value) const {
+    glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+  }
+
+  void set(const std::string &name, const float x, const float y, const float z) const {
+    glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+  }
+
+  void set(const std::string &name, const glm::vec4 &value) const {
+    glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+  }
+
+  void set(const std::string &name, const float x, const float y, const float z, const float w) const {
+    glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
+  }
+
+  void set(const std::string &name, const glm::mat2 &mat) const {
+    glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
+  }
+
+  void set(const std::string &name, const glm::mat3 &mat) const {
+    glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
+  }
+
+  void set(const std::string &name, const glm::mat4 &mat) const {
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
+  }
+
+private:
+  static void checkCompileErrors(const GLuint shader, const ShaderType type);
+
+  static uint compile(const std::string &code, ShaderType type);
+};
+
+} // namespace App
