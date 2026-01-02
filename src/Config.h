afd4e92befa7d96436681afc60fcc1d17f1af0ef
Michael Pacheco <mdpgd95@gmail.com>
2026-01-02 15:24:45 -0300

    refactor: nest namespace

--- src/Config.h
+++ src/Config.h
@@ -1,10 +1,10 @@
 #pragma once
 
-namespace App::Config {
-namespace Window {
+namespace App::Config::Window {
+
 constexpr auto TITLE = "Minecraft";
 constexpr int WIDTH = 1024;
 constexpr int HEIGHT = 768;
 inline float CLEAR_COLOR[] = {0.1, 0.1, 0.1};
-} // namespace Window
-} // namespace App::Config
+
+} // namespace App::Config::Window
