f90456c14d347e13b84394ae38b0bb9db1bfe4db
Michael Pacheco <mdpgd95@gmail.com>
2026-01-02 09:26:13 -0300

    feat: add logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+
 #include "Constants.h"
 #include "Engine.h"
 #include "Window.h"
@@ -8,6 +11,8 @@
 #include <SDL3/SDL_main.h>
 
 using namespace App;
+
+auto logger = spdlog::stdout_color_mt("bootstrap");
 
 constexpr Engine *asEngine(void *appstate) {
   auto *engine = static_cast<Engine *>(appstate);
@@ -23,20 +28,22 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   SDL_SetAppMetadata("Minecraft", "0.1.0", "com.example.minecraft");
 
   if (!SDL_Init(SDL_INIT_VIDEO)) {
-    SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+    logger->error("Couldn't initialize SDL: {}", SDL_GetError());
     return SDL_APP_FAILURE;
   }
 
   if (!SDL_CreateWindowAndRenderer(
           "Minecraft", Config::Window::WIDTH, Config::Window::HEIGHT,
           SDL_WINDOW_RESIZABLE, &window.m_sdlWindow, &window.m_sdlRenderer)) {
-    SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+    logger->critical("Couldn't create window/renderer: {}", SDL_GetError());
     return SDL_APP_FAILURE;
   }
 
   SDL_SetRenderLogicalPresentation(window.m_sdlRenderer, Config::Window::WIDTH,
                                    Config::Window::HEIGHT,
                                    SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+  logger->info("SDL initialized successfully");
 
   return SDL_APP_CONTINUE;
 }
@@ -65,4 +72,10 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
   return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result) {}
+void SDL_AppQuit(void *appstate, const SDL_AppResult result) {
+  if (result == SDL_APP_SUCCESS) {
+    logger->info("Application finished successfully");
+  } else {
+    logger->error("Application finished with error(s): {}", SDL_GetError());
+  }
+}
